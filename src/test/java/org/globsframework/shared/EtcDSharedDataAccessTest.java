9c2fb89f86b1f54afa035fef6bb13e6579738243
MarcGuiot <marc.guiot@free.fr>
2022-03-30 19:02:29 +0200

    Test for revision

--- src/test/java/org/globsframework/shared/EtcDSharedDataAccessTest.java
+++ src/test/java/org/globsframework/shared/EtcDSharedDataAccessTest.java
@@ -44,6 +44,62 @@ public class EtcDSharedDataAccessTest {
 
     @Test
     @Ignore("integration test to be filtered later")
+    public void getUnderAndListenWithEmpty() throws ExecutionException, InterruptedException, TimeoutException {
+        Client client = Client.builder().endpoints(ETCD).build();
+
+        SharedDataAccess etcDSharedDataAccess = EtcDSharedDataAccess.createBin(client);
+
+        MutableGlob data = Data1.TYPE.instantiate()
+                .set(Data1.shop, "mg.the-oz.com")
+                .set(Data1.workerName, "w1")
+                .set(Data1.num, 1)
+                .set(Data1.someData, "blabla");
+
+        etcDSharedDataAccess.register(data)
+                .get(1, TimeUnit.MINUTES);
+        etcDSharedDataAccess.delete(Data1.TYPE, data);
+
+        BlockingQueue<Glob> puts = new ArrayBlockingQueue<>(10);
+        BlockingQueue<Glob> deletes = new ArrayBlockingQueue<>(10);
+        CompletableFuture<List<Glob>> res = new CompletableFuture<>();
+        SharedDataAccess.ListenerCtrl listenerCtrl = etcDSharedDataAccess.getAndListenUnder(Data1.TYPE, FieldValues.EMPTY, new Consumer<List<Glob>>() {
+            public void accept(List<Glob> globs) {
+                etcDSharedDataAccess.register(data.duplicate().set(Data1.num, 3));
+                res.complete(globs);
+            }
+        }, new SharedDataAccess.Listener() {
+            public void put(Glob glob) {
+                puts.add(glob);
+            }
+
+            public void delete(Glob glob) {
+                deletes.add(glob);
+            }
+        }).get();
+
+        List<Glob> globs = res.get(10, TimeUnit.SECONDS);
+        Assert.assertEquals(0, deletes.size());
+        Assert.assertEquals(0, globs.size());
+//        Assert.assertEquals(1, globs.get(0).get(Data1.num).intValue());
+
+        data.set(Data1.num, 2);
+        etcDSharedDataAccess.register(data)
+                .get(1, TimeUnit.MINUTES);
+
+        Glob poll = puts.poll(10, TimeUnit.SECONDS);
+        Assert.assertNotNull(poll);
+        Assert.assertEquals(3, poll.get(Data1.num).intValue());
+        Glob poll1 = puts.poll(10, TimeUnit.SECONDS);
+        Assert.assertNotNull(poll1);
+        Assert.assertEquals(2, poll1.get(Data1.num).intValue());
+        listenerCtrl.close();
+//        deleteAll(client);
+//        client.close();
+    }
+
+
+    @Test
+    @Ignore("integration test to be filtered later")
     public void getUnderAndListen() throws ExecutionException, InterruptedException, TimeoutException {
         Client client = Client.builder().endpoints(ETCD).build();
 

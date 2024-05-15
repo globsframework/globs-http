e32e03d01642a91c088e715ec3238fff6e29d284
MarcGuiot <marc.guiot@free.fr>
2024-05-15 22:31:37 +0200

    Refactoring: remove GlobList

--- src/main/java/org/globsframework/shared/EtcDSharedDataAccess.java
+++ src/main/java/org/globsframework/shared/EtcDSharedDataAccess.java
@@ -145,7 +145,7 @@ public class EtcDSharedDataAccess implements SharedDataAccess {
         ByteSequence k = ByteSequence.from(path, StandardCharsets.UTF_8);
         ByteSequence v = ByteSequence.from(serializer.write(glob));
 
-        return kv.put(k, v, PutOption.newBuilder().withLeaseId(unLeaser.getLeaseId()).build()).thenApply(putResponse -> null);
+        return kv.put(k, v, PutOption.builder().withLeaseId(unLeaser.getLeaseId()).build()).thenApply(putResponse -> null);
     }
 
     public CompletableFuture<UnLeaser> registerWithLease(Glob glob, Duration duration) {
@@ -159,7 +159,7 @@ public class EtcDSharedDataAccess implements SharedDataAccess {
                 .thenApply(LeaseGrantResponse::getID)
                 .thenCompose(leaseId -> {
                     LOGGER.info("register " + path + " with lease id" + leaseId);
-                    return kv.put(k, v, PutOption.newBuilder().withLeaseId(leaseId).build())
+                    return kv.put(k, v, PutOption.builder().withLeaseId(leaseId).build())
                             .thenApply(putResponse -> new UnLeaser() {
                                 public void touch() {
                                     LOGGER.debug("Touch call on " + leaseId);
@@ -242,7 +242,7 @@ public class EtcDSharedDataAccess implements SharedDataAccess {
 
     private CompletableFuture<ResultAndRevision> getUnderWithRevision(GlobType type, FieldValues path) {
         CompletableFuture<GetResponse> getResponseCompletableFuture =
-                kv.get(ByteSequence.from(extractPath(prefix, path, type, separator), StandardCharsets.UTF_8), GetOption.newBuilder().isPrefix(true).build());
+                kv.get(ByteSequence.from(extractPath(prefix, path, type, separator), StandardCharsets.UTF_8), GetOption.builder().isPrefix(true).build());
         CompletableFuture<ResultAndRevision> completableFuture = getResponseCompletableFuture.thenApply(getResponse -> {
             List<KeyValue> kvs = getResponse.getKvs();
             long revision = getResponse.getHeader().getRevision();
@@ -319,7 +319,7 @@ public class EtcDSharedDataAccess implements SharedDataAccess {
         LOGGER.info("listenUnder " + orderedPath);
         Listener logListener = new LoggerListener(listener);
         GlobDeserializer.Deserializer globBinReader = deserializer.with(GlobTypeResolver.from(type));
-        WatchOption.Builder option = WatchOption.newBuilder()
+        WatchOption.Builder option = WatchOption.builder()
                 .withPrevKV(true)
                 .isPrefix(true);
         if (startAtRevision != -1) {
